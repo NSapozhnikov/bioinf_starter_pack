f46a339d755d3e6f3ae0fa5d0bbdf16dad097a8c
Nikita <nikita.sapozhnikov1@gmail.com>
2023-10-06 16:34:00 +0300

    Add FASTQ_TOOL

---
+++ Scripts/DNA_RNA_TOOLS.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print('DNA_RNA_TOOLS')
+
+
+if __name__ == '__main__':
+    pass
---
+++ Scripts/prototool.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print('prototool')
+
+
+if __name__ == '__main__':
+    pass
